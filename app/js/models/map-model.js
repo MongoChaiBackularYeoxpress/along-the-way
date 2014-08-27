19d814630bcfe3f868936eccbae9960f5299703a
Matt <mattclise@hotmail.com>
2014-08-27 14:58:57 -0700

    refactor map-view into model

--- app/js/models/map-model.js
+++ app/js/models/map-model.js
@@ -3,13 +3,105 @@ var Backbone = require('backbone');
 Backbone.$ = require('jquery');
 
 module.exports = Backbone.Model.extend({
+  
   defaults: {
-    position: '',
-    map: ''
+    startPoint: '',
+    endPoint: '',
+    map: '',
+    category:'',
+    position:'',
+    mapOptions: {zoom: 15},
   },
 
-  findLocations: function (position) {
-    console.log('this is our findLocations function in models.');
+  getLocation: function(){
+    var self = this;
+    if(!self.get('startPoint')){
+      if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var originPoint = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+          self.set('startPoint', originPoint);
+          console.log(self.get('startPoint'));
+        });
+      } // todo : err check
+    }
+    self.set('endPoint', new google.maps.LatLng(45.5500806, -122.6767286));
+  },
+
+  calcRoute: function(start, end, directionsDisplay, directionsService, service) {
+    var self = this;
+    var request = {
+              origin: start,
+              destination: end,
+              travelMode: google.maps.TravelMode.DRIVING
+          };
+    directionsService.route(request, function(response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        var polyline = new google.maps.Polyline({
+                path: [],
+                strokeWeight: 0
+              });
+        var bounds = new google.maps.LatLngBounds();
+        var numCount = 0;
+        var legs = response.routes[0].legs;
+        for (i=0;i<legs.length;i++) {
+          var steps = legs[i].steps;
+          for (j=0;j<steps.length;j++) {
+            var nextSegment = steps[j].path;
+            for (k=0;k<nextSegment.length;k++) {
+              polyline.getPath().push(nextSegment[k]);
+              bounds.extend(nextSegment[k]);
+
+              self.searchPoint(numCount, polyline, service);
+              numCount++;
+            }
+          }
+        }
+        var map = self.get('map');
+        polyline.setMap(map);
+        map.fitBounds(bounds);
+
+        directionsDisplay.setDirections(response);
+      }
+    });
+  },
+
+  searchPoint: function(numCount, polyline, service) {
+    var self = this;
+    if((numCount % 10) == 0){
+      // (function(index){
+        var requestLoc = {
+            location: polyline.getPath().getAt(numCount),
+            radius: 500,
+            types: ['bar']
+            // todo types: model.get(category)
+        };
+        setTimeout(function(){
+          service.nearbySearch(requestLoc, function(results, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+              for (var i = 0; i < results.length; i++) {
+                self.createMarker(results[i]);
+              }
+            }
+          });
+        }, numCount * 10);
+      // })(numCount);
+    }
+  },
+
+  createMarker: function(place, model) {
+    var self = this;
+    // console.log(self);
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: self.get('map'),
+      animation: google.maps.Animation.DROP,
+      position: place.geometry.location
+    });
+    // console.log(marker);
+    // google.maps.event.addListener(marker, 'click', function() {
+    //   this.infowindow.setContent(place.name);
+    //   this.infowindow.open(self.map, this);
+    // });
   }
 
   // startingPoint: function (start){}
